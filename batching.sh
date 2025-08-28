3443bc19fa3d7471be821a54239e00352919a5a3
AndrewWhyNot <101574299+projects-why-not@users.noreply.github.com>
2025-08-28 14:36:56 +0300

    Added batching script

---
+++ batching.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# consts
+SRC_TRIAL_DIR="/Users/andreyignatov/Downloads"  # TODO: change
+DST_RESULT_DIR="/Users/andreyignatov/Downloads"  # TODO: change
+SERVICE_PORT=3000  # TODO: m.b. change
+SERVICE_HOST="http://127.0.0.1"  # TODO: m.b. change
+
+# BATCH CODE
+for filename in $SRC_TRIAL_DIR/nsup_*_test.json; do
+	curl -d $filename $SERVICE_HOST:$SERVICE_PORT/nutrition -o $DST_RESULT_DIR/$filename_out.json
+done
+
+
+
