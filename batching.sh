70b794c082d20b39f568c6ac004825ce7f4b24f3
AndrewWhyNot <101574299+projects-why-not@users.noreply.github.com>
2025-08-28 14:41:50 +0300

    Fixed src path template in batch script

--- batching.sh
+++ batching.sh
@@ -7,7 +7,7 @@ SERVICE_PORT=3000  # TODO: m.b. change
 SERVICE_HOST="http://127.0.0.1"  # TODO: m.b. change
 
 # BATCH CODE
-for filename in $SRC_TRIAL_DIR/nsup_*_test.json; do
+for filename in $SRC_TRIAL_DIR/*.json; do
 	curl -d $filename $SERVICE_HOST:$SERVICE_PORT/nutrition -o $DST_RESULT_DIR/$filename_out.json
 done
 
